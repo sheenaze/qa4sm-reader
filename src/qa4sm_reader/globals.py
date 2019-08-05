465b4877de11202fbbd055faa28efc689aae11c8
Lukas Racbhauer <lukas.rachbauer@student.tuwien.ac.at>
2019-08-05 13:15:06 +0200

    Restructure and comment globals.py

--- src/qa4sm_reader/globals.py
+++ src/qa4sm_reader/globals.py
@@ -7,19 +7,74 @@ __license__ = "mit"
 
 
 """
-Global variables collected from different sources.
+Settings and global variables collected from different sources. 
 """
 
-import cartopy.crs as ccrs  # TODO: is it meaningful to create prjojection objects here?
 
-# === from qa4sm.validator.validation.graphics ===
+import cartopy.crs as ccrs
 
+
+# === plot defaults ===
+matplotlib_ppi = 72  # Don't change this, it's a matplotlib convention.
+index_names = ['lat', 'lon']  # Names used for 'lattitude' and 'longitude' coordinate.
+dpi = 100  # Resolution in which plots are going to be rendered.
+max_title_len = 50  # maximum length of plot title in chars. if longer, it will be broken in multiple lines.
+title_pad = 12.0  # Padding below the title in points. default padding is matplotlib.rcParams['axes.titlepad'] = 6.0
+data_crs = ccrs.PlateCarree()  # Default map projection. use one of
+
+# === map plot defaults ===
+scattered_datasets = ['ISMN']  # dataset names which require scatterplots (data is scattered in lat/lon)
+map_figsize = [11.32, 6.10]  # size of the output figure in inches.
+naturalearth_resolution = '110m'  # One of '10m', '50m' and '110m'. Finer resolution slows down plotting. see https://www.naturalearthdata.com/
+crs = ccrs.PlateCarree()  # projection. Must be a class from cartopy.crs. Note, that plotting labels does not work for most projections.
+markersize = 4  # diameter of Marker in points.
+map_pad = 0.15  # padding relative to map height.
+grid_intervals = [0.25, 0.5, 1, 2, 5, 10, 30]  # grid spacing in degree to choose from (plotter will try to make 5 gridlines in the smaller dimension)
+
+# === boxplot defaults ===
+boxplot_printnumbers = True  # Print 'median', 'nObs', 'stdDev' to the boxplot.
+boxplot_figsize = [6.30, 4.68]  # size of the output figure in inches.
+
+# === watermark defaults ===
+watermark = u'made with QA4SM (qa4sm.eodc.eu)'  # Watermark string
+watermark_pos = 'top'  # Default position ('top' or 'bottom')
+watermark_fontsize = 10  # fontsize in points (matplotlib uses 72ppi)
+watermark_pad = 5  # padding above/below watermark in points (matplotlib uses 72ppi)
+
+# === colormaps used for plotting metrics ===
+# Colormaps can be set for classes of similar metrics or individually for metrics.
+# Any colormap name can be used, that works with matplotlib.pyplot.cm.get_cmap('colormap')
+# more on colormaps: https://matplotlib.org/users/colormaps.html | https://morphocode.com/the-use-of-color-in-maps/
+# colorcet: http://colorcet.pyviz.org/user_guide/Continuous.html
+
+_cclasses = {
+    'div_better': 'RdYlBu',  # diverging: 1 good, 0 special, -1 bad (pearson's R, spearman's rho')
+    'div_neutr': 'RdYlGn',  # diverging: zero good, +/- neutral: (bias)
+    'seq_worse': 'cet_CET_L4_r',  # sequential: increasing value bad (p_R, p_rho, rmsd, ubRMSD, RSS):
+    'seq_better': 'cet_CET_L4'  # sequential: increasing value good (n_obs)
+}
+
+_colormaps = {  # from /qa4sm/validator/validation/graphics.py
+    'R': _cclasses['div_better'],
+    'p_R': _cclasses['seq_worse'],
+    'rho': _cclasses['div_better'],
+    'p_rho': _cclasses['seq_worse'],
+    'rmsd': _cclasses['seq_worse'],
+    'bias': _cclasses['div_neutr'],
+    'n_obs': _cclasses['seq_better'],
+    'ubRMSD': _cclasses['seq_worse'],
+    'mse': _cclasses['seq_worse'],
+    'mse_corr': _cclasses['seq_worse'],
+    'mse_bias': _cclasses['seq_worse'],
+    'mse_var': _cclasses['seq_worse'],
+}
+
+# Value ranges of metrics
 _metric_value_ranges = {  # from /qa4sm/validator/validation/graphics.py
     'R': [-1, 1],
     'p_R': [0, 1],  # probability that observed corellation is statistical fluctuation
     'rho': [-1, 1],
     'p_rho': [0, 1],
-    # probability that observed corellation is statistical fluctuation (probability of null hypothesis: there is no correlation)
     'rmsd': [0, None],
     'bias': [None, None],
     'n_obs': [0, None],
@@ -56,12 +111,9 @@ _metric_units = {  # from /qa4sm/validator/validation/graphics.py
     'ASCAT': r'percentage of saturation',
     'SMAP': r'm^3 m^{-3}',
     'ERA5': r'm^3 m^{-3}',
-    'ERA': r'm^3 m^{-3}',  # by mistake, the dataset was called like this for some time.
     'ESA_CCI_SM_combinded': r'',
     'SMOS': r''
 }
-
-# === from qa4sm.validator.validation.globals ===
 
 # label name for all metrics
 _metric_name = {  # from /qa4sm/validator/validation/globals.py
@@ -80,36 +132,8 @@ _metric_name = {  # from /qa4sm/validator/validation/globals.py
     'mse_var': 'Mean square error variance',
 }
 
-# colormaps used for plotting metrics
-# more on colormaps: https://matplotlib.org/users/colormaps.html | https://morphocode.com/the-use-of-color-in-maps/
-# colorcet: http://colorcet.pyviz.org/user_guide/Continuous.html
-
-_cclasses = {
-    'div_better': 'RdYlBu',
-    # diverging: 1 good, 0 special, -1 bad (pearson's R, spearman's rho') 'cet_coolwarm_r', 'cet_CET_D8_r'],
-    'div_neutr': 'RdYlGn',
-    # diverging: zero good, +/- neutral: (bias):  'Spectral', 'cet_CET_D3', 'cet_gwv', 'cet_bwy', 'cet_bjy'],
-    'seq_worse': 'cet_CET_L4_r',  # sequential: increasing value bad (p_R, p_rho, rmsd, ubRMSD, RSS, ):
-    'seq_better': 'cet_CET_L4'  # sequential: increasing value good (n_obs):
-}
-
-_colormaps = {  # from /qa4sm/validator/validation/graphics.py
-    'R': _cclasses['div_better'],
-    'p_R': _cclasses['seq_worse'],
-    'rho': _cclasses['div_better'],
-    'p_rho': _cclasses['seq_worse'],
-    'rmsd': _cclasses['seq_worse'],
-    'bias': _cclasses['div_neutr'],
-    'n_obs': _cclasses['seq_better'],
-    'ubRMSD': _cclasses['seq_worse'],
-    'mse': _cclasses['seq_worse'],
-    'mse_corr': _cclasses['seq_worse'],
-    'mse_bias': _cclasses['seq_worse'],
-    'mse_var': _cclasses['seq_worse'],
-}
-
-# === pretty names ===
-# pretty names for all datasets
+# === pretty names for datasets ===
+# Note: ncplot.get_meta tries to retrieve pretty names from the global attributes and falls back to here.
 _dataset_pretty_names = {  # from qa4sm\validator\fixtures\datasets.json
     'ISMN': r'ISMN',
     'C3S': r'C3S',
@@ -117,7 +141,6 @@ _dataset_pretty_names = {  # from qa4sm\validator\fixtures\datasets.json
     'ASCAT': r'H-SAF ASCAT SSM CDR',
     'SMAP': r'SMAP level 3',
     'ERA5': r'ERA5',
-    'ERA': r'ERA5',  # by mistake, the dataset was called like this for some time.
     'ESA_CCI_SM_combined': r'ESA CCI SM combined',
     'SMOS': r'SMOS IC'
 }
@@ -138,31 +161,3 @@ _dataset_version_pretty_names = {  # from qa4sm\validator\fixtures\versions.json
     "ERA5_test": "ERA5 test",
     "ERA5": "ERA5"
 }
-
-# === plot defaults ===
-matplotlib_ppi = 72
-index_names = ['lat', 'lon']
-dpi = 100
-max_title_len = 50  # maximum length of plot title in chars. if longer, it will be broken in multiple lines.
-title_pad = 12.0  # in points. default padding is matplotlib.rcParams['axes.titlepad'] = 6.0
-data_crs = ccrs.PlateCarree()
-
-# === map plot defaults ===
-scattered_datasets = ['ISMN']  # dataset names which require scatterplots (data is scattered in lat/lon)
-map_figsize = [11.32, 6.10]
-naturalearth_resolution = '110m'  # choose from '10m', '50m' and '110m'
-crs = ccrs.PlateCarree()
-markersize = 4  # diameter in points.
-map_pad = 0.15  # padding relative to map height.
-grid_intervals = [0.25, 0.5, 1, 2, 5, 10,
-                  30]  # grid delta to choose from (plotter will try to make 5 gridlines in the smaller dimension)
-
-# === boxplot defaults ===
-boxplot_printnumbers = True
-boxplot_figsize = [6.30, 4.68]
-
-# === watermark defaults ===
-watermark = u'made with QA4SM (qa4sm.eodc.eu)'
-watermark_pos = 'top'
-watermark_fontsize = 10  # in points
-watermark_pad = 5  # in points (matplotlib uses 72ppi)
